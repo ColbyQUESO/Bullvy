a61cef0db6088c55be5198a45f2e2019ebea7dc1
Colby Curtis <justanothercolby@outlook.com>
2018-01-11 15:38:56 -0700

    Initial Commit

---
+++ Bullvy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bullvy.Startup))]
+namespace Bullvy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
